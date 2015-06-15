e14f8cb56f6b3cdcfe0cecacd5668d65071ffe6e
Saroar <saroar9@gmail.com>
2015-06-15 15:00:39 +0300

    style style lets see if its correct

--- black_jack.rb
+++ black_jack.rb
@@ -7,7 +7,7 @@ def calculate_total(cards)
   total = 0
     arr.each do |value|
       if value == "Ace"
-     	  total += 11
+        total += 11
       elsif value.to_i == 0 # J, Q, K
         total += 10
       else
@@ -82,23 +82,22 @@ if my_total == 21
 end
 
 while my_total < 21
-  puts "What #{name} like to do? Hit/Stay => click (h/s?) and enter !"
+  puts "What #{name} like to do? Hit/Stay => click (h/s?) and enter."
   hit_or_stay = gets.chomp.downcase
 
   if !['h', 's'].include?(hit_or_stay)
-  puts "Error! #{name} must enter only H/S"
-  next
-end
+    puts "Error! #{name} must enter only H/S"
+    next
+  end
 
-if hit_or_stay == "s"
-  puts "#{name} choose stay"
-  puts ""
-  break
-end
+  if hit_or_stay == "s"
+    puts "#{name} choose stay"
+    puts ""
+    break
+  end
 
   #hit
   new_card = deck.pop
-  #puts "=> Dealing card to player: #{new_card}  other card in hidded "
   puts "Dealing card to player"
   puts "=> #{new_card.join(" of ")}"
   puts "=> other card is hidden "
@@ -112,23 +111,23 @@ end
 
 #Buest
 
-if my_total == 21
-  puts "Congratulation #{name} HIT blackjack! #{name} you win!"
-  elsif my_total > 21
-  puts "Sorry, it looks like #{name} you buested"
-  exit
- end
+  if my_total == 21
+    puts "Congratulation #{name} HIT blackjack! #{name} you win!"
+    elsif my_total > 21
+    puts "Sorry, it looks like #{name} you buested"
+    exit
+  end
 end
 
 
 # Dealer turn
-if dealer_total == 21
-  puts "Sorry, dealer hit blackjack #{name} you lose!"
-  exit
-end
+  if dealer_total == 21
+    puts "Sorry, dealer hit blackjack #{name} you lose!"
+    exit
+  end
 
 while dealer_total < 17
-  # Hit
+# Hit
   new_card = deck.pop
   puts "Dealing new card for dealer: "
   puts "=> #{new_card.join(" of ")}"
@@ -137,12 +136,12 @@ while dealer_total < 17
   puts "Dealer toatal is now: #{dealer_total}"
   puts ""
 
-if dealer_total == 21
-  puts "Sorry,Dealer hit blackjack, #{name} you lose"
-  exit
+  if dealer_total == 21
+    puts "Sorry,Dealer hit blackjack, #{name} you lose"
+    exit
   elsif dealer_total > 21
-  puts "Congratulation,dealer buested! #{name} you win!"
-  exit
+    puts "Congratulation,dealer buested! #{name} you win!"
+    exit
   end
 end
 
@@ -162,14 +161,13 @@ end
   puts ""
 
 
-if dealer_total > my_total
-  puts "Sorry, dealer win!"
-  elsif dealer_total < my_total
-  puts "Congratulation #{name} you wins!"
-  else
-  puts "Its tie!"
-end
-
+  if dealer_total > my_total
+    puts "Sorry, dealer win!"
+    elsif dealer_total < my_total
+    puts "Congratulation #{name} you wins!"
+    else
+    puts "Its tie!"
+  end
 end
 
 puts "\nThanks for playing black jack #{name}"
